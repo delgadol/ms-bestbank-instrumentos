deabd019c8716332aa8768634cab7f85aaaddade
Luis Delgado <37703993+delgadol@users.noreply.github.com>
2023-07-18 15:57:47 -0500

    Initial commit

---
+++ src/main/java/com/bestbank/instrumentos/MsBestbanckInstrumentosApplication.java
@@ -0,0 +1,13 @@
+package com.bestbank.instrumentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsBestbanckInstrumentosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsBestbanckInstrumentosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bestbank/instrumentos/MsBestbanckInstrumentosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bestbank.instrumentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsBestbanckInstrumentosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
